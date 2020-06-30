051dbc83690589a28ca1504c45169b02c0bb17d9
Ilushkins <sn0wy-0wl@yandex.ru>
2020-06-30 17:39:41 +0500

    Basic async requests

---
+++ SMSActivate/RequestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace SMSActivate
+{
+    public class RequestResult
+    {
+        public string value;
+        public HttpResponseMessage Response;
+
+        public RequestResult(HttpResponseMessage httpResponseMessage)
+        {
+            Response = httpResponseMessage;
+            value = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
---
+++ SMSActivate/SMSActivate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SMSActivate
+{
+    public class SMSActivateClient
+    {
+        public string APIKey { get; private set; }
+        public readonly HttpClient HttpClient;
+        public readonly string DefaultRoute = "https://sms-activate.ru/stubs/handler_api.php";
+
+        public SMSActivateClient(string APIKey)
+        {
+            this.APIKey = APIKey;
+            HttpClient = new HttpClient();
+        }
+
+        public void ChangeAPIKey(string APIKey)
+        {
+            this.APIKey = APIKey;
+        }
+
+        public async Task<RequestResult> Request(Dictionary<string, string> requestContent)
+        {
+            HttpContent content = new FormUrlEncodedContent(requestContent);
+            var Response = await HttpClient.PostAsync(DefaultRoute, content);
+            return new RequestResult(Response);
+        }
+
+        public async Task<RequestResult> Request(KeyValuePair<string, string> requestContent)
+        {
+            HttpContent content = new FormUrlEncodedContent(new[] { requestContent });
+            var Response = await HttpClient.PostAsync(DefaultRoute, content);
+            return new RequestResult(Response);
+        }
+
+        public async Task<RequestResult> Request(KeyValuePair<string, string>[] requestContent)
+        {
+            HttpContent content = new FormUrlEncodedContent(requestContent);
+            var Response = await HttpClient.PostAsync(DefaultRoute, content);
+            return new RequestResult(Response);
+        }
+
+        public RequestResult GetBalance()
+        {
+            var response = Request(new[] { new KeyValuePair<string, string>("api_key", APIKey), new KeyValuePair<string, string>("action", "getBalance") });
+            response.Wait();
+            return response.Result;
+        }
+    }
+}
