659cdae8b878b16bda5eaf59a51978bf39a63388
Ilushkins <sn0wy-0wl@yandex.ru>
2020-06-30 20:38:19 +0500

    GetNumber methods

--- SMSActivate/SMSActivate.cs
+++ SMSActivate/SMSActivate.cs
@@ -10,6 +10,7 @@ namespace SMSActivate
         public string APIKey { get; private set; }
         public readonly HttpClient HttpClient;
         public readonly string DefaultRoute = "https://sms-activate.ru/stubs/handler_api.php";
+        public readonly string ReferalLink = "https://sms-activate.ru/?ref=421489";
 
         public SMSActivateClient(string APIKey)
         {
@@ -22,32 +23,65 @@ namespace SMSActivate
             this.APIKey = APIKey;
         }
 
-        public async Task<RequestResult> Request(Dictionary<string, string> requestContent)
+        public async Task<RequestResult> GetNumberAsync(string service, bool isForward = false)
+        {
+            Dictionary<string, string> content = new Dictionary<string, string>();
+            content.Add("api_key", APIKey);
+            content.Add("action", "getNumber");
+            content.Add("isForward", BooleanToString(isForward));
+            content.Add("ref", ReferalLink);
+            content.Add("service", service);
+            
+            var response = await Request(content);
+            return response;
+        }
+
+        public RequestResult GetNumber(string service, bool isForward = false)
+        {
+            var task = GetNumberAsync(service, isForward);
+            task.Wait();
+            return task.Result;
+        }
+
+        public void GetNumber(Service service, bool isForward = false) => GetNumber(ServiceInfo.GetServiceId(service), isForward);
+
+        public Task<RequestResult> GetBalanceAsync()
+        {
+            return Request(new[] { new KeyValuePair<string, string>("api_key", APIKey), new KeyValuePair<string, string>("action", "getBalance") });
+        }
+
+        public RequestResult GetBalance()
+        {
+            var response = GetBalanceAsync();
+            response.Wait();
+            return response.Result;
+        }
+
+        private string BooleanToString(bool value) => (value) ? "0" : "1";
+
+        #region Requests
+
+        private async Task<RequestResult> Request(Dictionary<string, string> requestContent)
         {
             HttpContent content = new FormUrlEncodedContent(requestContent);
             var Response = await HttpClient.PostAsync(DefaultRoute, content);
             return new RequestResult(Response);
         }
 
-        public async Task<RequestResult> Request(KeyValuePair<string, string> requestContent)
+        private async Task<RequestResult> Request(KeyValuePair<string, string> requestContent)
         {
             HttpContent content = new FormUrlEncodedContent(new[] { requestContent });
             var Response = await HttpClient.PostAsync(DefaultRoute, content);
             return new RequestResult(Response);
         }
 
-        public async Task<RequestResult> Request(KeyValuePair<string, string>[] requestContent)
+        private async Task<RequestResult> Request(KeyValuePair<string, string>[] requestContent)
         {
             HttpContent content = new FormUrlEncodedContent(requestContent);
             var Response = await HttpClient.PostAsync(DefaultRoute, content);
             return new RequestResult(Response);
         }
 
-        public RequestResult GetBalance()
-        {
-            var response = Request(new[] { new KeyValuePair<string, string>("api_key", APIKey), new KeyValuePair<string, string>("action", "getBalance") });
-            response.Wait();
-            return response.Result;
-        }
+        #endregion
     }
 }
---
+++ SMSActivate/ServiceInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMSActivate
+{
+    public enum Country
+    {
+        Russia = 0,
+        Ukraine,
+        Kazahstan,
+        China,
+        Philipines,
+    }
+
+    public enum Service
+    {
+        Vkontakte,
+        Telegram,
+        Google,
+        MailRu,
+        AnyOther
+    }
+
+    public enum Operators
+    {
+        Megafon,
+        Mts,
+        BeeLine
+    }
+
+    public static class ServiceInfo
+    {
+        static string a = "a";
+
+        private static Dictionary<Service, string> _servicesId = new Dictionary<Service, string>()
+        {
+            {Service.Vkontakte, "vk"}, {Service.Telegram, "tg"}, {Service.AnyOther, "ot"}, {Service.Google, "go"},
+            {Service.MailRu, "ma" }
+        };
+
+        public static string GetServiceId(Service serviceName) => _servicesId[serviceName];
+    }
+}
