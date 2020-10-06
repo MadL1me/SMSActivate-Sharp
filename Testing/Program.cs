a7fd8ccf2ab1004a37dbaac2a64b1527e44a686c
Ilushkins <sn0wy-0wl@yandex.ru>
2020-10-06 15:35:56 +0300

    Tests

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -7,7 +7,15 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            
+            var activate = new SMSActivateClient("A30503A622ef26d89533867f1ee4df29");
+            var b = activate.GetNumberAsync(Service.AnyOther);
+            //var c = activate.SetActivationStatus(281216893, ActivationStatus.ReadyForSms);
+            b.Wait();
+            var c = activate.SetActivationStatus(b.Result.Phone.Id, ActivationStatus.ReadyForSms);
+            c.Wait();
+            //Console.WriteLine(a.Result);
+            Console.WriteLine(b.Result.Phone.Number);
+            Console.WriteLine(c.Result.Result);
         }
     }
 }
